122e695e41801b8a9bb590a399114b201ef3a300
kst0204 <g2172043@tcu.ac.jp>
2023-06-22 14:02:01 +0900

    [Add]

---
+++ test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(1+2)
+
+
+# In[5]:
+
+
+#コメントアウト
+1+2
+
+
+# In[3]:
+
+
+print('hello world')
+
+
+# # こんにちは
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
